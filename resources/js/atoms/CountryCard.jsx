5acc964cedbd4ff9c0ccd4f14d26d664b49af136
Lukas Riha <lukas.riha94@seznam.cz>
2020-09-16 14:59:54 +0200

    Added CountryCards and CountryCardContainer and their respective dark mode themes.

--- resources/js/atoms/CountryCard.jsx
+++ resources/js/atoms/CountryCard.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const CountryCard = (props) => {
-  const { flag, name, population, capital, url } = props;
+  const { flag, name, population, capital, url, region } = props;
 
   return (
     <div className="CountryCard">
-      <img src={flag} alt={`${name} flag`} />
-      <p>{name}</p>
-      <p>{population}</p>
-      <p>{capital}</p>
-      <p>{url}</p>
+      <Link to={url}>
+        <img src={flag} alt={`${name} flag`} className="CountryCard__Flag" />
+        <div className="CountryCard__Body">
+          <h3 className="CountryCard__Name">{name}</h3>
+
+          <div className="CountryCard__Row">
+            <b>Population:</b>
+            {population}
+          </div>
+          <div className="CountryCard__Row">
+            <b>Region</b>
+            {region}
+          </div>
+          <div className="CountryCard__Row">
+            <b>Capital:</b>
+            {capital}
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
@@ -19,6 +34,7 @@ CountryCard.propTypes = {
   flag: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   population: PropTypes.number.isRequired,
+  region: PropTypes.string.isRequired,
   capital: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
 };
--- resources/js/molecules/CountryCardContainer.jsx
+++ resources/js/molecules/CountryCardContainer.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import CountryCard from '@Atoms/CountryCard';
 
 const CountryCardContainer = (props) => {
-  console.log(props);
+  const { countries } = props;
 
-  return <div className="CountryCardContainer">Hello :)</div>;
+  return (
+    <div className="CountryCardContainer">
+      {Array.isArray(countries) &&
+        countries.length > 0 &&
+        countries.map((country) => (
+          <CountryCard
+            name={country.name}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+            url={`/${country.alpha2Code}`}
+            flag={country.flag}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default CountryCardContainer;
