afb789ceb0a4404a40a32ee6e85fae229f8887c6
Lukas Riha <lukas.riha94@seznam.cz>
2020-09-25 14:42:23 +0200

    Added suspense

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,46 +1,45 @@
-import React, { /*  useEffect, useState, */ Suspense } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from '@Molecules/Layout';
 import ErrorPage from '@Routes/ErrorPage';
-/* import Overlay from '@Atoms/Overlay'; */
-import CountryPage from '@Routes/CountryPage';
+import Overlay from '@Atoms/Overlay';
 import Loading from '@Atoms/Loading';
 
 const Homepage = React.lazy(() => import('./routes/Homepage'));
+const CountryPage = React.lazy(() => import('./routes/CountryPage'));
 
 // Contexts
 import { ThemeProvider } from '@Contexts/ThemeContext';
 import { CountriesProvider } from '@Contexts/CountriesContext';
 
 function App() {
-  /*   const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     setIsLoaded(true);
-  }, [isLoaded]); */
+  }, [isLoaded]);
 
   return (
     <Router>
       <CountriesProvider>
         <ThemeProvider>
-          {/*   <Overlay visible={isLoaded ? false : true} /> */}
+          <Overlay visible={isLoaded ? false : true} />
           <Layout>
-            {/* <Loading /> */}
-            <Switch>
-              <Route exact path="/country/:code">
-                <CountryPage />
-              </Route>
+            <Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/country/:code">
+                  <CountryPage />
+                </Route>
 
-              <Route exact path="/">
-                <Suspense fallback={<Loading />}>
+                <Route exact path="/">
                   <Homepage />
-                </Suspense>
-              </Route>
+                </Route>
 
-              <Route path="*">
-                <ErrorPage />
-              </Route>
-            </Switch>
+                <Route path="*">
+                  <ErrorPage />
+                </Route>
+              </Switch>
+            </Suspense>
           </Layout>
         </ThemeProvider>
       </CountriesProvider>
